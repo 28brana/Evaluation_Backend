14108fcaa75600caf036b2b0185dc202f33656fe
28brana <28brana@gmail.com>
2022-06-20 18:11:30 +0530

    bug fixed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const Map=require("./model/mapSchema");
 const db = require('./config/db');
+const cors = require('cors')
 
 const PORT=process.env.PORT || 5000;
 const app=express();
